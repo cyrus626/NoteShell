1117492cb292fa5f3863c2fa80a7d985dc9d179e
cyrus626 <56732915+cyrus626@users.noreply.github.com>
2021-03-26 11:59:46 +0100

    First Commit

---
+++ DataManager/DataManger.cs
@@ -0,0 +1,11 @@
+using System;
+namespace NoteShell
+{
+    class DataManager
+    {
+        //private string NoteName { get; set; }
+        //private int NoteId { get; set; }
+        //private DateTime NoteDateTime { get; set;}
+        //private string Content { get; set; }
+    }
+}
