c656695efa816da6d5f37d105b5ad2b4222b8e25
cyrus626 <56732915+cyrus626@users.noreply.github.com>
2021-03-27 11:51:37 +0100

    Added LiteDB package

---
+++ DBManager/DBManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace NoteShell
+{
+	public class DBManager
+	{
+		
+		//Construct
+		public DBmanager()
+		{
+		}
+		public DBManager(string Name, DateTime FileDate, string content)
+		{
+
+		}
+
+	}
+}
