0a311b981978b178be35433abedded7421465018
cyrus626 <56732915+cyrus626@users.noreply.github.com>
2021-03-29 15:57:06 +0100

    More datails added

--- Models/Index.cs
+++ Models/Index.cs
@@ -49,40 +49,8 @@ namespace NoteShell.Models.Index
                     break;
             }
         }
+        
         #region
-        public void CurrentNote(string noteName, int id)
-        {
-            
-            Console.WriteLine("Instruction: Enter the content of the note" +
-                "\n Press shift + enter to enter the next line \nAnd click enter when through");
-            Console.WriteLine($"----------------------{noteName}-Document-------------------------");
-            Console.WriteLine(currentNote.Content);
-            string newContent = Console.ReadLine();
-            string currentContent = (string)currentNote.Content;
-            string content = (newContent + "\n" + currentContent);
-            Console.Write("Do you wish to save document?(Y/N)> ");
-            string reply = Console.ReadLine();
-            
-            if (reply.ToLower() == "y")
-            {
-                
-                if (id == 0)
-                {
-                    Console.WriteLine("New note! click enter to save");
-                }
-                else
-                {
-                    Console.WriteLine($"Saving {noteName}");
-                }
-                Console.WriteLine($"{noteName} Saved");
-                SaveNote(noteName, content, id);
-            }
-            else
-            {
-                Console.WriteLine($"{noteName} Discarded");
-            }
-        }
-        #endregion
         public void SaveNote(string noteName, string content ,int id)
         {
             Note saveNote = new Note();
@@ -95,56 +63,133 @@ namespace NoteShell.Models.Index
             {
                 saveNote.Id = id;
             }
+            //Saving other segments of the note
             saveNote.NoteName = noteName;
             saveNote.Content = content;
             saveNote.TimeSaved = DateTime.Now;
             DBManager.SaveNote(saveNote);
             Console.WriteLine($"{noteName} saved");
         }
+        #endregion
+        #region
         public void ViewNote()
         {
+            Console.Write("Enter the note name> ");
+            string noteName = Console.ReadLine();
+            //Finding all notes in that with the key word
+            foreach (var note in DBManager.GetNotes())
+            {
+                if (note.NoteName == noteName)
+                {
+                    currentNote = DBManager.GetNoteByName(noteName);
+                    ViewCurrentNote(currentNote.Id);
+
+                }
+
+            }
+            Console.WriteLine("Error: File not found!");
             Console.WriteLine("Execise patient, Working in progress...");
         }
+        #endregion
+        #region
         public void ViewNotes()
         {
             //Finding all notes in database
-            Console.WriteLine("Id" + " " + "File name");
-            foreach (var note in DBManager.GetNotes() )
+            Console.WriteLine("Id" + "  " + "File name" + "     " + "Time saved" );
+            var allNotes = DBManager.GetNotes();
+            int numOfNotes = allNotes.Count;
+            foreach (var note in allNotes)
             {
-                int noteId = note.Id;
+                int eachNoteId = note.Id;
                 string noteName = note.NoteName;
-                Console.WriteLine(noteId + " " + noteName);
+                DateTime createdDate = note.TimeSaved;
+                Console.WriteLine(eachNoteId + "    " + noteName + "        " + createdDate);
             }
-            Console.WriteLine("Enter the Id to select the note"); 
-            ViewCurrentNote();
+            Console.WriteLine("Enter the Id number to select the note"); 
+            int noteId = Convert.ToInt32(Console.ReadLine());
+            ViewCurrentNote(noteId);
         }
-        public void ViewCurrentNote()
+        #endregion
+        #region
+        public void ViewCurrentNote(int noteId)
         {
             //Selecting the note
-            int noteId = Convert.ToInt32(Console.ReadLine());
-            currentNote = DBManager.GetNote(noteId);
-            Console.WriteLine("E. Edit note content \nD. Delete note \nB. Back to view all notes");
-            Console.Write("Option avialable is E > ");
-            string reply = Console.ReadLine();
-            if (reply.ToLower().Equals("e"))
+            foreach(var note in DBManager.GetNotes())
             {
-                //display the content of the note
-                
-                CurrentNote(currentNote.NoteName, noteId);
+                if(note.Id == noteId)
+                {
+                    currentNote = DBManager.GetNote(noteId);
+                    Console.WriteLine("E. Edit note content \nD. Delete note \nB. Back to view all notes");
+                    Console.Write("Option avialable is E > ");
+                    string reply = Console.ReadLine();
+                    if (reply.ToLower().Equals("e"))
+                    {
+                        //display the content of the note
+                        CurrentNote(currentNote.NoteName, noteId);
+                    }
+                    else if (reply.ToLower().Equals("d"))
+                    {
+                        Console.WriteLine("Execise patient, Working in progress...");
+                    }
+                    else if (reply.ToLower().Equals("b"))
+                    {
+                        Console.WriteLine("OK, Noted");
+                        ViewNotes();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You've choose the wrong option");
+                        Console.WriteLine("Execise patient, Working in progress...");
+                    }
+                    
+                }
+                 
             }
-            else if (reply.ToLower().Equals("d"))
+            Console.WriteLine("Error: File not found!");
+            ViewNotes();
+        }
+        #endregion
+        #region
+        public void CurrentNote(string noteName, int id)
+        {
+
+            Console.WriteLine("Instruction: Enter the content of the note" +
+                "\n Press shift + enter to enter the next line \nAnd click enter when through");
+            Console.WriteLine($"----------------------{noteName}-Document-------------------------");
+            Console.WriteLine(currentNote.Content);
+            string newContent = Console.ReadLine();
+            string content;
+            if ((string)currentNote.Content != "")//If it is not empty.
             {
-                Console.WriteLine("Execise patient, Working in progress...");
-            }
-            else if (reply.ToLower().Equals("b"))
-            {
-                Console.WriteLine("Execise patient, Working in progress...");
+                string currentContent = (string)currentNote.Content;
+                content = (currentContent + "\n" + newContent);//Adding up to the previous contents
             }
             else
             {
-                Console.WriteLine("You've choose the wrong option");
-                Console.WriteLine("Execise patient, Working in progress...");
+                content = newContent;
+            }
+            Console.Write("Do you wish to save document?(Y/N)> ");
+            string reply = Console.ReadLine();
+
+            if (reply.ToLower() == "y")
+            {
+
+                if (id == 0)
+                {
+                    Console.WriteLine("New note! click enter to save");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine($"Saving {noteName}");
+                }
+                SaveNote(noteName, content, id);
+            }
+            else
+            {
+                Console.WriteLine($"{noteName} Discarded");
             }
         }
+        #endregion
     }
 }
